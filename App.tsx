6a938e4e873b8fd5f485cc799e263ad05b337057
Jorge Terence <jorge.terence@outlook.com>
2021-12-16 13:39:30 -0300

    Basic tab navigation

--- App.tsx
+++ App.tsx
@@ -1,6 +1,39 @@
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 import HomeScreen from './app/screens/HomeScreen';
+import IndexScreen from './app/screens/IndexScreen';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default () => (
-	<HomeScreen />
+	<NavigationContainer>
+		<Tab.Navigator
+			initialRouteName="Início"
+			shifting={true}
+			barStyle={{ backgroundColor: '#3B7FBA' }}
+			screenOptions={({ route }) => ({
+				tabBarIcon: () => {
+					// Setup tab icons
+					type PossibleIcons =
+						| 'home'
+						| 'list'
+						| 'map'
+						| 'information-circle'
+						| 'settings';
+					let iconName: PossibleIcons = 'settings';
+					if (route.name == 'Início') iconName = 'home';
+					else if (route.name == 'Índex') iconName = 'list';
+					else if (route.name == 'Mapa') iconName = 'map';
+					else if (route.name == 'Info') iconName = 'information-circle';
+
+					return <Ionicons name={iconName} color={'white'} size={22} />;
+				},
+			})}
+		>
+			<Tab.Screen name="Início" component={HomeScreen} />
+			<Tab.Screen name="Índex" component={IndexScreen} />
+		</Tab.Navigator>
+	</NavigationContainer>
 );
---
+++ app/screens/IndexScreen.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+type Animal = { name: string; alt: string };
+
+const animals: Animal[] = [
+	{ name: 'Capivara', alt: 'Hydrochoerus hydrochaeris' },
+	{ name: 'Onça-pintada', alt: 'Panthera onca' },
+	{ name: 'Tamanduá-bandeira', alt: 'Myrmecophaga tridactyla' },
+	{ name: 'Cobra-coral', alt: 'Micrurus frontalis' },
+	{ name: 'Téiu', alt: 'Salvator merianae' },
+	{ name: 'Cágado-de-barbicha', alt: 'Phrynops geoffroanus' },
+];
+
+export default () => (
+	<View style={styles.container}>
+		<Text style={styles.title}>Índex de Animais</Text>
+		<FlatList data={animals} renderItem={ListItem} keyExtractor={({ name }) => name} />
+	</View>
+);
+
+type ListItemProps = { item: Animal };
+
+const ListItem = ({ item }: ListItemProps) => (
+	<View style={styles.listItem}>
+		<Text>{item.name}</Text>
+		<Text>{item.alt}</Text>
+	</View>
+);
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+		paddingTop: 12,
+	},
+	listItem: {
+		padding: 6,
+		backgroundColor: '#51C8B0',
+		alignItems: 'flex-start',
+		justifyContent: 'center',
+		borderRadius: 10,
+		shadowColor: 'black',
+		elevation: 2,
+		marginBottom: 6,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+		textAlign: 'center',
+		marginBottom: 16,
+	},
+});
