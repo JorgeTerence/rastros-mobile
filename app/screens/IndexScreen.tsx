7cb1eac9591e4ac893d9484d7bf6c5cb18bef53c
Jorge Terence <jorge.terence@outlook.com>
2021-12-18 17:16:38 -0300

    Added IndexItem.tsx
    Exported ListItem from IndexScreen to its own file.

--- app/screens/IndexScreen.tsx
+++ app/screens/IndexScreen.tsx
@@ -1,17 +1,10 @@
 import { collection, doc, getDoc } from 'firebase/firestore/lite';
 import React, { useContext, useEffect, useState } from 'react';
-import {
-	FlatList,
-	Image,
-	StyleSheet,
-	Text,
-	View,
-	TouchableNativeFeedback
-} from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { FirestoreContext } from '../../firebaseConfig';
 import Body from '../components/Body';
-import { palette } from '../theme/colors';
-import type { Animal } from '../types/types'
+import IndexItem from '../components/IndexItem';
+import type { Animal } from '../types/types';
 
 const dummyData: Animal[] = [];
 
@@ -31,70 +24,31 @@ export default ({ navigation }: Props) => {
 				<Text style={styles.title}>Índex de Animais</Text>
 				<FlatList
 					data={data}
-					renderItem={({ item }) => <ListItem item={item} action={() => navigation.navigate('Data', item)} />}
+					renderItem={({ item }) => (
+						<IndexItem
+							item={item}
+							action={() => navigation.navigate('Data', item)}
+						/>
+					)}
 					keyExtractor={({ name }) => name}
-					style={{
-						width: '100%',
-						paddingTop: 5,
-						marginBottom: 45,
-						paddingBottom: 10,
-					}}
+					style={styles.list}
 				/>
 			</View>
 		</Body>
 	);
 };
 
-type ListItemProps = { item: Animal, action: () => void };
-
-const ListItem = ({ item, action }: ListItemProps) => {
-	// TODO: Make this responsive to each and every collection, not just animals
-	let imageSource;
-	if (item.asset == 'Mamífero') imageSource = require('../../assets/mammal.png');
-	else if (item.asset == 'Réptil') imageSource = require('../../assets/reptile.png');
-	else if (item.asset == 'Ave') imageSource = require('../../assets/bird.png');
-	return (
-		<TouchableNativeFeedback onPress={action} >
-			<View style={styles.listItem}>
-				<Image source={imageSource} style={styles.classIcon} />
-				<View>
-					<Text style={{ fontWeight: 'bold', fontSize: 16 }}>
-						{item.name}
-					</Text>
-					<Text style={{ fontSize: 13, color: '#333' }}>{item.sub}</Text>
-				</View>
-			</View>
-		</TouchableNativeFeedback>
-	);
-};
-
 const styles = StyleSheet.create({
-	classIcon: {
-		height: 36,
-		width: undefined,
-		aspectRatio: 1,
-		borderRadius: 18,
-		marginRight: 15,
-		marginLeft: 5,
-		resizeMode: 'contain',
-	},
-	listItem: {
-		padding: 12.5,
-		backgroundColor: palette.sand,
-		alignItems: 'center',
-		justifyContent: 'flex-start',
-		borderRadius: 15,
-		shadowColor: 'black',
-		elevation: 3,
-		marginBottom: 12,
-		width: '85%',
-		alignSelf: 'center',
-		flexDirection: 'row',
-	},
 	title: {
 		fontSize: 20,
 		fontWeight: 'bold',
 		textAlign: 'center',
 		marginBottom: 16,
+	},
+	list: {
+		width: '100%',
+		paddingTop: 5,
+		marginBottom: 45,
+		paddingBottom: 10,
 	},
 });
