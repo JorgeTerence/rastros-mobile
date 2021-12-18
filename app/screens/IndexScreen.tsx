ed60ccb2484daf707ec7cbfcbf8d05821096d2b9
Jorge Terence <jorge.terence@outlook.com>
2021-12-18 14:59:25 -0300

    Added assets to indicate each animal classification

--- app/screens/IndexScreen.tsx
+++ app/screens/IndexScreen.tsx
@@ -1,6 +1,12 @@
 import { collection, doc, getDoc } from 'firebase/firestore/lite';
 import React, { useContext, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import {
+	FlatList,
+	Image,
+	StyleSheet,
+	Text,
+	View
+} from 'react-native';
 import { FirestoreContext } from '../../firebaseConfig';
 import Body from '../components/Body';
 import { palette } from '../theme/colors';
@@ -25,7 +31,12 @@ export default () => {
 					data={data}
 					renderItem={ListItem}
 					keyExtractor={({ name }) => name}
-					style={{ width: '100%', paddingTop: 5, marginBottom: 35, paddingBottom: 10 }}
+					style={{
+						width: '100%',
+						paddingTop: 5,
+						marginBottom: 45,
+						paddingBottom: 10,
+					}}
 				/>
 			</View>
 		</Body>
@@ -34,25 +45,49 @@ export default () => {
 
 type ListItemProps = { item: Animal };
 
-const ListItem = ({ item }: ListItemProps) => (
-	<View style={styles.listItem}>
-		<Text>{item.name}</Text>
-		<Text>{item.sub}</Text>
-	</View>
-);
+const ListItem = ({ item }: ListItemProps) => {
+	// TODO: Make this responsive to each and every collection, not just animals
+	let imageSource;
+	if (item.asset == 'Mamífero')
+		imageSource = require('../../assets/mammal.png');
+	else if (item.asset == 'Réptil')
+		imageSource = require('../../assets/reptile.png');
+	else if (item.asset == 'Ave') imageSource = require('../../assets/bird.png');
+	return (
+		<View style={styles.listItem}>
+			<Image source={imageSource} style={styles.classIcon} />
+			<View>
+				<Text style={{ fontWeight: 'bold', fontSize: 16 }}>
+					{item.name}
+				</Text>
+				<Text>{item.sub}</Text>
+			</View>
+		</View>
+	);
+};
 
 const styles = StyleSheet.create({
+	classIcon: {
+		height: 36,
+		width: undefined,
+		aspectRatio: 1,
+		borderRadius: 18,
+		marginRight: 15,
+		marginLeft: 5,
+		resizeMode: 'contain',
+	},
 	listItem: {
-		padding: 10,
+		padding: 12.5,
 		backgroundColor: palette.sand,
-		alignItems: 'flex-start',
-		justifyContent: 'center',
-		borderRadius: 14,
+		alignItems: 'center',
+		justifyContent: 'flex-start',
+		borderRadius: 15,
 		shadowColor: 'black',
-		elevation: 2,
+		elevation: 3,
 		marginBottom: 12,
-		width: '80%',
+		width: '85%',
 		alignSelf: 'center',
+		flexDirection: 'row',
 	},
 	title: {
 		fontSize: 20,
