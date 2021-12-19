f7e5d770dd81447bb024a5f4c10cc64c36dc7914
Jorge Terence <jorge.terence@outlook.com>
2021-12-19 10:50:32 -0300

    Revert "Prepared for shipping for Expo"
    This reverts commit c3106bcbc6f151fe18b54b901abdc7ecad38c5ad.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,16 +1,25 @@
+import {
+	API_KEY,
+	APP_ID,
+	AUTH_DOMAIN,
+	MEASUREMENT_ID,
+	MESSAGING_SENDER_ID,
+	PROJECT_ID,
+	STORAGE_BUCKET
+} from '@env';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 import { getStorage } from 'firebase/storage';
 import { createContext } from 'react';
 
 const firebaseConfig = {
-	apiKey: 'AIzaSyCERhGjW44z8SeVPWV2-Sejjjk2S2EhEvE',
-	authDomain: 'rastros-silvestres-e6837.firebaseapp.com',
-	projectId: 'rastros-silvestres-e6837',
-	storageBucket: 'rastros-silvestres-e6837.appspot.com',
-	messagingSenderId: '624863156234',
-	appId: '1:624863156234:web:76457497ad3056849167f3',
-	measurementId: 'G-MWQCLT4MVY',
+	apiKey: API_KEY,
+	authDomain: AUTH_DOMAIN,
+	projectId: PROJECT_ID,
+	storageBucket: STORAGE_BUCKET,
+	messagingSenderId: MESSAGING_SENDER_ID,
+	appId: APP_ID,
+	measurementId: MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
